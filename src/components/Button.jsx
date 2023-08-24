098ff7dc90ca4ef28ef8be0a83d64cdb3e96d6a7
Bikrant Jajware <bikrantjajware@gmail.com>
2023-08-24 16:16:04 +0530

    add hero component

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Button = ({ label, iconURL}) => {
+  return (
+    <button className='flex items-center justify-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red
+                        rounded-full border-coral-red text-white' >
+      {label}
+      <img className='ml-2 rounded-full w-5 h-5' src={iconURL} alt="arrow-right" />
+    </button >
+  )
+}
+
+export default Button
