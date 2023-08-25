d79bcc1413057854c5ee1132a90d4cf9938127b7
Bikrant Jajware <bikrantjajware@gmail.com>
2023-08-25 20:07:50 +0530

    started popular products

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { products } from '../constants'
+
 
 const PopularProducts = () => {
   return (
-    <div>PopularProducts</div>
+    <section id='products' className='max-container max-sm:mt-12'>
+      <div className="flex flex-start justify-start gap-5">
+
+        <h2>Popular Products</h2>
+        <p>Experience top notch quality and style with out sought after collections. Discover a world of comfort, design and value</p>
+      </div>
+    </section>
   )
 }
 
-export default PopularProducts
+export default PopularProducts 
