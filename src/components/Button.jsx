1d01bad708280a7bb9ebcf189debf508c09f9346
Bikrant Jajware <bikrantjajware@gmail.com>
2023-08-26 16:23:23 +0530

    fix button and add .gitignore

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,7 @@ const Button = ({ label, iconURL}) => {
     <button className='flex items-center justify-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red
                         rounded-full border-coral-red text-white' >
       {label}
-      <img className='ml-2 rounded-full w-5 h-5' src={iconURL} alt="arrow-right" />
+      {iconURL && <img className='ml-2 rounded-full w-5 h-5' src={iconURL} alt="arrow-right" />}
     </button >
   )
 }
