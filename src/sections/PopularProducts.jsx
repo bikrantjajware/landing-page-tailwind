c5f236e6fbf6fd66742de3a52e9d6f1e6172cf84
Bikrant Jajware <bikrantjajware@gmail.com>
2023-08-26 16:25:28 +0530

    add PopularProducts component

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { products } from '../constants'
-
+import PopularProductCard from '../components/PopularProductCard';
 
 const PopularProducts = () => {
   return (
     <section id='products' className='max-container max-sm:mt-12'>
       <div className="flex flex-start justify-start gap-5">
+        <h2 className='font-palanquin font-bold text-4xl'> Our <span className='text-coral-red'>Popular</span> Products</h2>
+        <p className='lg:max-w-lg font-montserrat text-slate-gray'>Experience top notch quality and style with out sought after collections. Discover a world of comfort, design and value</p>
+      </div>
 
-        <h2>Popular Products</h2>
-        <p>Experience top notch quality and style with out sought after collections. Discover a world of comfort, design and value</p>
+      <div className='mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14'>
+        {products.map( product => ( <PopularProductCard key={product.name} {...product} />))}
       </div>
     </section>
   )
